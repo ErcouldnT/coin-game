29fa93e23af18bd267de324efe4d5201f3fbf730
Ercode <ercode@pm.me>
2022-05-16 22:11:05 +0300

    Mongoose User error

--- client/src/_db.js
+++ client/src/_db.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 
-import User from './models/User.js';
+import User from './models/user.js';
 
 mongoose.connect(process.env.MONGO_URI, (error) => {
   console.log(error
